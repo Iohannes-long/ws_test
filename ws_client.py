90a3789d609fe9c28730969f623ab2f86b8ffb48
adlong <3140618@163.com>
2024-06-01 16:55:00 +0800

    update client

--- ws_client.py
+++ ws_client.py
@@ -1,14 +1,20 @@
+import websocket
+import _thread
+import time
+import rel
 import argparse
-import signal
 import json
-from websockets.sync.client import connect
 
-_app_exit = False
-def _quit(signum, frame):
-    global _app_exit
-    _app_exit =True
-    print('waiting to close')
-    
+
+_config = {
+    'url': None,
+    'file': None,
+    'split': None,
+    'be_json': True,
+    'be_first': True,
+    'qty': -1,
+}
+
 def _my_print(pf, data, be_first, split_flag, be_json):
     if pf is not None:
         if be_first:
@@ -26,44 +32,65 @@ def _my_print(pf, data, be_first, split_flag, be_json):
         print(data)
         
 def _my_print_end(pf, be_json):
+    if pf is None:
+        return
     if be_json:
         pf.write(f']')
 
-def main(url, outfile, num, split_flag, be_json):
-    global _app_exit
+def on_message(ws, data):
+    global _config
     
-    qty = -1
-    if num is not None:
-        qty = num
-    pf = None
-    if outfile is not None:
-        pf = open(outfile, "w")
-    with connect(url) as session:
-        print(f'has connected {url}')
-        be_first = True
-        while not _app_exit and qty != 0:
-            data = session.recv()
-            if be_json:
-                parsed_data = json.loads(data)
-                data = json.dumps(parsed_data, indent=4, sort_keys=False)            
-            _my_print(pf, data, be_first, split_flag, be_json)
-            if qty > 0:
-                qty -= 1
-            be_first = False
-    if pf is not None:
-        _my_print_end(pf, be_json)
-        pf.close()
+    if _config['be_first']:
+        parsed_data = json.loads(data)
+        data = json.dumps(parsed_data, indent=4, sort_keys=False)            
+    _my_print(_config['file'], data, _config['be_first'], _config['split'], _config['be_json'])
+    if _config['qty'] is not None and _config['qty'] > 0:
+        _config['qty'] -= 1
+    else:
+        ws.close()
+    _config['be_first'] = False
 
-if __name__ == '__main__':
-    signal.signal(signal.SIGINT, _quit)
-    signal.signal(signal.SIGTERM, _quit)
+def on_error(ws, error):
+    print(error)
+
+def on_close(ws, close_status_code, close_msg):
+    global _config
+    _my_print_end(_config['file'], _config['be_first'])
+    print("### closed ###")
+
+def on_open(ws):
+    print("Opened connection")
     
+def main(url, file, split, be_json, qty):    
+    global _config
+    _config['url'] = url
+    _config['file'] = file
+    _config['split'] = split
+    _config['be_json'] = be_json
+    _config['qty'] = qty
+    
+    if file is not None and file != '':
+         pf = open(file, "w")
+         _config['file'] = pf
+    
+    websocket.enableTrace(False)
+    ws = websocket.WebSocketApp(url,
+                              on_open=on_open,
+                              on_message=on_message,
+                              on_error=on_error,
+                              on_close=on_close)
+
+    ws.run_forever(dispatcher=rel, reconnect=5)
+    rel.signal(2, rel.abort)
+    rel.dispatch()
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-u', dest='url', type=str, default='ws://127.0.0.1', help='ws://x.x.x.x[:y]/z')
-    parser.add_argument('-f', dest='outfile', type=str, default=None, help='file name output data to')
+    parser.add_argument('-u', dest='url', type=str, default='http://127.0.0.1', help='ws://x.x.x.x[:y]/z')
+    parser.add_argument('-f', dest='file', type=str, default=None, help='the file to send')
+    parser.add_argument('-s', dest='split', type=str, default=None, help='split flag with two data')
+    parser.add_argument('-j', dest='json', type=int, default=1, help='the file is json')
     parser.add_argument('-n', dest='num', type=int, default=None, help='the num of receive server data times')
-    parser.add_argument('-s', dest='split', type=str, default='@@@', help='split flag of two data')
-    parser.add_argument('-j', dest='json', type=int, default=1, help='data is json or not')
     args = parser.parse_args()
     be_json = True if args.json == 1 else False
-    main(args.url, args.outfile, args.num, args.split, be_json)
+    main(args.url, args.file, args.split, be_json, args.num)
